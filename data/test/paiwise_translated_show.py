855a48dfaa28697940e5a07ea9b1a90d07ff6594
StarWalkin <sjtu.cs.gy@gmail.com>
2023-12-06 14:20:29 +0800

    bilingual release revised

--- data/test/paiwise_translated_show.py
+++ data/test/paiwise_translated_show.py
@@ -1,13 +1,13 @@
 import json
 
 # with open ("./pairwise_merged.jsonl", "r") as file:
-with open("./zh_chatgpt_reference.jsonl") as file:
+with open("./zh_reference_chatgpt_critique.jsonl") as file:
     data = file.readlines()
 
 for single_data in data:
     single_data = json.loads(single_data)
     # with open ("./bilin_paiwise_translated_show.txt", "a") as f:
     # with open ("/cpfs01/user/liupengfei/yguo/zn_autoj/autoj/data/outputs/gpt4_comparison_show.txt", "a") as f:
-    with open("./zh_chatgpt_reference_show.txt", "a") as f:
+    with open("./_show1.txt", "a") as f:
         print (single_data, file=f)
 
